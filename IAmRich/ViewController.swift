aefd2fd23265a89a69aabb8dcff8d7a0e5dcfad2
AltynbekDinmukhamed <a.dimash.a_03@mail.ru>
2023-01-29 08:15:08 +0600

    img

--- IAmRich/ViewController.swift
+++ IAmRich/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: -Creating Views-
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "I Am Rich"
+        label.font = .systemFont(ofSize: 30)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let image: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "diamond")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    //MARK: -Life cycle-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpViews()
     }
 
+    //MARK: -Funcitons-
+    private func setUpViews() {
+        view.backgroundColor = .systemBlue
+        view.addSubview(image)
+        NSLayoutConstraint.activate([
+            image.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.widthAnchor.constraint(equalToConstant: view.frame.width / 2)
+        ])
+        
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.widthAnchor.constraint(equalToConstant: view.frame.width / 2)
+        ])
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,9 @@
+#  Description
+
+this is the very first marathon project. in
+
+The task was just to get acquainted with XCode and programming
+
+
+
+![Screenshot 1](/IAmRich/screenShots/The i am rich.png)
